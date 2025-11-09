194a34cbbe7827cdadc9dc0f278f4544d9c76306
Emad <emadakroushwork@gmail.com>
2025-11-10 00:29:15 +0330

    add the new for font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ import * as path from "path";
 export default defineNuxtConfig({
   modules: ["@primevue/nuxt-module"],
   css: [
+    "@/assets/styles/global.css",
     "@/assets/styles/tailwind.css",
     "@/assets/styles/base.css",
     "@mdi/font/css/materialdesignicons.min.css",
